7041d424dc9b1150fe3267ab9a92c10c7fae8eef
Xavi Ibáñez Martínez <241xavi@gmail.com>
2020-11-12 18:53:04 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,12 +6,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="{{asset('css/app.css')}}">
         <title>{{config('app.name', 'LSAPP')}}</title>
-
         
     </head>
     <body>
         @include('inc.navbar')
         <div class="container">
+            @include('inc.messages')
             @yield('content')
         </div>
     </body>
