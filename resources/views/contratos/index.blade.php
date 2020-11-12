3b298f9162aabfd6c63a20fdf71a06f72f282b3f
Xavi Ibáñez Martínez <241xavi@gmail.com>
2020-11-12 17:57:50 +0100

    Update index.blade.php

--- resources/views/contratos/index.blade.php
+++ resources/views/contratos/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <h1>Contratos</h1>
-    @if(count($contratos) > 1)
+    @if(count($contratos) > 0)
         @foreach ($contratos as $contrato)
             <div class="well">
                 <h4><a href="/lsapp/public/contratos/{{$contrato->id}}">{{$contrato->id}} {{$contrato->nombre_contrato}}</a></h4>
