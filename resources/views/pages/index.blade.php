ac82e0c1317787fbaadd5fc2a265436a8b095195
Xavi Ibáñez Martínez <241xavi@gmail.com>
2020-11-12 10:37:42 +0100

    layout

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,15 +1,5 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>{{config('app.name', 'LSAPP')}}</title>
-
-        
-    </head>
-    <body>
+@section('content')
         <p>Test</p>
-    </body>
-</html>
+@endsection
