8896939e197106356e5af8a42b5b1c2061c5984b
Xavi Ibáñez Martínez <241xavi@gmail.com>
2020-11-12 19:11:55 +0100

    Update create.blade.php

--- resources/views/contratos/create.blade.php
+++ resources/views/contratos/create.blade.php
@@ -4,12 +4,16 @@
     <h1>Crear Contrato</h1>
     {!! Form::open(['action' => 'ContratosController@store', 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
         <div class="form-group">
-            {{Form::label('nombre', 'Nombre')}}
-            {{Form::text('nombre', '', ['class' => 'form-control', 'placeholder' => 'Nombre'])}}
+            {{Form::label('nombre_contrato', 'Nombre del contrato')}}
+            {{Form::text('nombre_contrato', '', ['class' => 'form-control', 'placeholder' => 'Nombre del contrato'])}}
         </div>
         <div class="form-group">
-            {{Form::label('idCliente', 'ID del cliente')}}
-            {{Form::text('idCliente', '', ['id' => 'article-ckeditor', 'class' => 'form-control', 'placeholder' => 'ID del cliente'])}}
+            {{Form::label('id_cliente', 'ID del cliente')}}
+            {{Form::text('id_cliente', '', ['id' => 'article-ckeditor', 'class' => 'form-control', 'placeholder' => 'ID del cliente'])}}
+        </div>
+        <div class="form-group">
+            {{Form::label('nombre_cliente', 'Nombre del cliente')}}
+            {{Form::text('nombre_cliente', '', ['class' => 'form-control', 'placeholder' => 'Nombre del cliente'])}}
         </div>
         {{Form::submit('Submit', ['class'=>'btn btn-primary'])}}
     {!! Form::close() !!}
