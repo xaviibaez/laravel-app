60e79979df3f8a2b196f87e714527242f2925d83
Xavi Ibáñez Martínez <241xavi@gmail.com>
2020-11-14 16:54:59 +0100

    auth
    auth

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-inverse">
+<nav class="navbar navbar-default">
     <div class="container">
         <div class="navbar-header">
 
@@ -23,10 +23,39 @@
             </ul>
 
             <ul class="nav navbar-nav">
-              <li><a href="/lsapp/public">Home</a></li>
-              <li><a href="/lsapp/public/about">Sobre Mi</a></li>
-              <li><a href="/lsapp/public/contratos">Contratos</a></li>
-              <!--<li><a href="/lsapp/public/contratos/create">Crear contrato</a></li>-->
+                <li><a href="/lsapp/public">Home</a></li>
+                <li><a href="/lsapp/public/about">Sobre Mi</a></li>
+                <li><a href="/lsapp/public/contratos">Contratos</a></li>
+                <!--<li><a href="/lsapp/public/contratos/create">Crear contrato</a></li>-->
+              </ul>
+
+            <!-- Right Side Of Navbar -->
+            <ul class="nav navbar-nav navbar-right">
+                <!-- Authentication Links -->
+                @if (Auth::guest())
+                    <li><a href="{{ route('login') }}">Login</a></li>
+                    <li><a href="{{ route('register') }}">Register</a></li>
+                @else
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
+
+                        <ul class="dropdown-menu" role="menu">
+                            <li>
+                                <a href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                             document.getElementById('logout-form').submit();">
+                                    Logout
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    {{ csrf_field() }}
+                                </form>
+                            </li>
+                        </ul>
+                    </li>
+                @endif
             </ul>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ Route::get('/about', 'PagesController@about');
 Route::get('/services', 'PagesController@services');
 
 Route::resource('contratos', 'ContratosController');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
